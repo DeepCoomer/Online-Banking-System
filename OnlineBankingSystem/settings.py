3da7952b7531392bb490a01b944a75e23fa0c864
DeepCoomer <deepcoomer198@gmail.com>
2021-04-19 20:12:14 +0530

    Final Commit

--- OnlineBankingSystem/settings.py
+++ OnlineBankingSystem/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY ="oy6)s%lldal4@w@+2x2bgiwrko_5+o!h&y0tuwf=p%z6l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
